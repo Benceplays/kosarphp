d3638fdd6a240fecaf700315b154d08a0ca9cdaa
Script Devil <71324001+Benceplays@users.noreply.github.com>
2023-02-10 20:54:36 +0100

    asdas

---
+++ javascript.js
@@ -0,0 +1,6 @@
+function informationon() {
+    const information = document.getElementById('information').style.display = "block";
+}
+function informationoff() {
+    const information = document.getElementById('information').style.display = "none";
+}
